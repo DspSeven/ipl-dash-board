397171378ff9f089b71d9fb75355e6c2f825c430
DspSeven <119715565+DspSeven@users.noreply.github.com>
2022-12-24 13:27:22 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,12 +1,13 @@
 // Write your code here
 // Write your code here
+// Write your code here
 import './index.css'
 
 const NotFound = () => {
   console.log('no page found')
   return (
-    <div>
-      <h1>Page Not Found!!</h1>
+    <div className="not-found-bg-container">
+      <h1 className="para">Page Not Found !!</h1>
     </div>
   )
 }
